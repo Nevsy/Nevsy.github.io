a746deb8812df5a117167ccb3de24d713f7ed636
Nev <sven@royer.se>
2023-10-08 13:05:25 +0200

    deploy

--- vite.config.js
+++ vite.config.js
@@ -16,6 +16,13 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
   },
-  base: '/Nevsy.github.io/',
-  plugins: [vue()]
+  // base: '/Nevsy.github.io/',
+  // plugins: [vue()]
 })
+
+/* module.exports = {
+  publicPath: process.env.NODE_ENV === 'production'
+    ? '/Nevsy.github.io/'
+    : '/'
+}
+*/
